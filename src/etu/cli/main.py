73c1a63ad278aab7d31e2635bdbe6f9b5c32b398
lemlnn <lemuel.lin056@gmail.com>
2026-09-15 19:36:33 -0500

    fix: old version bump from 0.1.3 to 0.1.4
    - too many files to change versioning... maybe should make that unified-

--- src/etu/cli/main.py
+++ src/etu/cli/main.py
@@ -27,7 +27,7 @@ def launch_gui():
 def main():
     while True:
         print()
-        print("ETU dev-0.1.3")
+        print("ETU dev-0.1.4")
         print()
         print("[1] Inventory")
         print("[2] Universe")
